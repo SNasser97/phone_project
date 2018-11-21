26909f5cebb8447ad4927d44f3fce39e5ab09c97
Nasser Ahmed <37514151+SNasser97@users.noreply.github.com>
2018-11-21 14:01:55 +0000

    added condition for accessory

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const resetBtn = document.querySelector('#reset');
 
 /*===================== GET VALUES =======================*/
 function getBalance() { //give value to number
-  const balance = document.querySelector('#balance');
-  return Number(balance.value);
+  const ba = document.querySelector('#bal');
+  return Number(ba.value);
 }
 
 function getSpendLmt() {
   const spendLimit = document.querySelector('#spend-limit');
-  return Number(spendLimit.value);
+  return spendLimit.setAttribute('value',Number(spendLimit.value));
 }
 
 function getPhonePrice() {
@@ -34,28 +34,34 @@ function getAccPrice() {
   return Number(accessoryPrice.value);
 }
 
-function getAmount() {
-  const amount = 0;
+function getAmt() {
+  let amount = 0;
   return amount;
 }
 
 /*===================== CONDITIONAL =======================*/
+function addOnAcc(a,b) {
+  let amt = getAmt(); //0
+  return amt += a + b; //amt = 0 + phoneprc + accPrc
+}
 
-function addAccPrice() {
-  if(getPhonePrice() < getSpendLmt()) {
-    //add accprice to phone price
+function calcAddOn() {
+  let amt = getAmt();
+  let addOnAcc = addOnAcc(getPhonePrice(), getAccPrice());
+  if(getPhonePrice() < getSpendLmt()) { // phone cheap than spendlmt then we add on accssry
+     return amt + addOnAcc;
   }
-    //else only give phonePrice
+    return amt+=getPhonePrice(); //return just phoneprc
 }
 
 function keepBuyingPhone() {
-  /* while (balance > amt) {
-    we keep going
-  } else {
-    we STOP
-  }
-  balance = balance - amt;
-  */
+  while (getBalance() > amt) {
+    //we keep going till condition is false
+    if(getBalance() < amt) {
+      //show no. of phones I can buy
+    }
+     //balance = balance - amt;
+  } 
 }
 
 function calc() { //+ can add as string, make sure its int
@@ -76,9 +82,13 @@ function isInputValid(a,b,c,d,) { //balance | spendLimit | phonePrc | AccPrc
   return false;
 }
 
-/*===================== CALCULATE =======================*/
+/*===================== OUTPUT/RESULT ======================*/
 function giveOutput() { //if input is NaN for amt/phone/accessory
-  if(isInputValid(getBalance(), getSpendLmt() ,getPhonePrice(), getAccPrice(),)) {
+  let bal = getBalance();
+  let spendLimit = getSpendLmt();
+  let phonePrice = getPhonePrice();
+  let accPrice = getAccPrice();
+  if(isInputValid(bal, spendLimit ,phonePrice, accPrice,)) {
     return output.textContent = 'Error! Enter valid money';
   }
     return calc(); //then input is not NaN so call calc
@@ -86,33 +96,34 @@ function giveOutput() { //if input is NaN for amt/phone/accessory
 
 /*===================== RESET VALUES =======================*/
 function reset() { 
- balance.value = 0; //works, even though inside function
- spendLimit.value = 0; //does not work, goes for rest except balance
+  debugger;
+ accessoryPrice.value = 0; //works, even though inside function
+ //spendLimit.value = 0; //does not work, goes for rest except balance
 }
 
-function reset2() { //This works when I cache DOM in global
-  balance.value = 0;
-  spendLimit.value = 0;
-  phonePrice.value = 0;
-  accessoryPrice.value = 0;
-}
-console.log(getBalance()); //call works
-console.log('balance value logged', balance.value = 0); //this is defined
+// function reset2() { //This works when I cache DOM in global
+//   balance.value = 0;
+//   spendLimit.value = 0;
+//   phonePrice.value = 0;
+//   accessoryPrice.value = 0;
+// }
+// console.log(getBalance()); //call works
+// console.log('balance value logged', balance.value = 0); //this is defined
 
-console.log(getSpendLmt()); //call works
-console.log('spendLimit value logged', spendLimit.value = 0); //Ref error: not defined
+// console.log(getSpendLmt()); //call works
+// console.log('spendLimit value logged', spendLimit.value = 0); //Ref error: not defined
 
-console.log(getPhonePrice()); //call works
-console.log('phonePrice value logged', phonePrice.value = 0); //Ref error: not defined
+// console.log(getPhonePrice()); //call works
+// console.log('phonePrice value logged', phonePrice.value = 0); //Ref error: not defined
 
-console.log(getAccPrice()); //call works
-console.log('accessoryPrice value logged', accessoryPrice.value = 0); //Ref error: not defined
+// console.log(getAccPrice()); //call works
+// console.log('accessoryPrice value logged', accessoryPrice.value = 0); //Ref error: not defined
 
 taxOutput.setAttribute('value', tax());
 calcBtn.addEventListener('click', giveOutput);
 resetBtn.addEventListener('click', reset);
 
 // output.textContent = '£' + parseFloat(0.00);
-//calcBtn.addEventListener('click', calc);
+// calcBtn.addEventListener('click', calc);
 
 //if value is number then change class of output to .true
